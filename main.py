407b8299c769b2dfa5392361d6e6c78965a74e19
doctorwho1110 <yeldanugur99@gmail.com>
2023-06-08 09:41:58 +0300

    Creating pages by using row["Pages"]

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ df=pd.read_csv("topics.csv")
 
 
 for index,row in df.iterrows():
-    pdf.add_page()
+        pdf.add_page()
 
-    pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
-             ln=1, border=0)
-    pdf.line(10,20,200,20)
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
+                 ln=1, border=0)
+        pdf.line(10, 20, 200, 20)
+
+        for i in range(row["Pages"]-1):
+            pdf.add_page()
+            #pdf.image("complete.png",10,30,w=0,h=12)
 
 pdf.output("output.pdf")
